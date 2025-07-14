4ae5fb7443319801db107b1dc01589cba2693ed7
STOPPRE <alexnvera@gmail.com>
2025-07-14 10:37:37 -0500

    chore: trigger final deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import MortgageCalculator from '@/components/MortgageCalculator'
  
-// Triggering deployment with a comment
+// Triggering deployment after setting cleanup policy
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
